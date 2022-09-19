9b08dcde8fbedf8e051ea90920cc52997b3237b1
6Cristian6Chile6 <cristian.perez.loezar@gmail.com>
2022-09-18 21:35:15 -0300

    tratando de entender hash

--- 05-EstructuraDeDatos-II/homework/homework.js
+++ 05-EstructuraDeDatos-II/homework/homework.js
@@ -88,7 +88,13 @@ La clase debe tener los siguientes métodos:
 Ejemplo: supongamos que quiero guardar {instructora: 'Ani'} en la tabla. Primero puedo chequear, con hasKey, si ya hay algo en la tabla con el nombre 'instructora'; luego, invocando set('instructora', 'Ani'), se almacenará el par clave-valor en un bucket específico (determinado al hashear la clave)
 */
 
-function HashTable() {}
+function HashTable() {
+  this.numBuckets=35;
+  //`set`, `get`, y `hasKey`
+  HashTable.prototype.set = function (){};
+  HashTable.prototype.get = function (){};
+  HashTable.prototype.hasKey = function (){};
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
